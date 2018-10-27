df9084b58716f359eb36839655d79559a62365e2
Max Helmetag <mhelmetag@gmail.com>
2018-10-26 23:56:06 -0700

    simple countdown timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,124 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pomodoro Clock',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new Home(title: 'Pomodoro Clock'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Home extends StatefulWidget {
   final String title;
+  final Duration tickDuration;
+
+  Home({this.title, this.tickDuration = const Duration(seconds: 1)});
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _HomeState createState() => new _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  Timer _timer;
+  int _currentTimeSeconds;
+  bool _running;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _currentTimeSeconds = 300;
+    _running = false;
+  }
+
+  @override
+  void dispose() {
+    stop();
+    super.dispose();
+  }
+
+  void start() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _running = true;
     });
+    this._timer = new Timer.periodic(widget.tickDuration, tick);
+  }
+
+  void stop() {
+    setState(() {
+      _running = false;
+    });
+    _timer.cancel();
+  }
+
+  void reset() {
+    setState(() {
+      _currentTimeSeconds = 300;
+    });
+  }
+
+  void tick(Timer time) {
+    if (_currentTimeSeconds <= 0) {
+      stop();
+    } else {
+      setState(() {
+        _currentTimeSeconds--;
+      });
+    }
+  }
+
+  String stringTime() {
+    var minutes = (_currentTimeSeconds ~/ 60);
+    var minutesString = minutes.toString().padLeft(2, '0');
+
+    var seconds = _currentTimeSeconds - (minutes * 60);
+    var secondsString = seconds.toString().padLeft(2, '0');
+
+    return "$minutesString:$secondsString";
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text(widget.title)
       ),
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
-              'You have pushed the button this many times:',
+              stringTime(),
+              style: TextStyle(fontSize: 50)
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new RaisedButton(
+                  onPressed: (_running && _currentTimeSeconds == 0) ? null : start,
+                  child: new Text('Start')
+                ),
+                new RaisedButton(
+                  onPressed: _running ? stop : null,
+                  child: new Text('Stop')
+                ),
+                new RaisedButton(
+                  onPressed: _running ? null : reset,
+                  child: new Text('Reset')
+                )
+              ],
+            )
           ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
+      )
     );
   }
 }
