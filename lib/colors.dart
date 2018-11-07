068aa31e65af5730bcd1e949857a0d98bd3cea26
Max Helmetag <mhelmetag@gmail.com>
2018-11-07 00:39:13 -0800

    add icons, add screenshots

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,5 +5,5 @@ const kOrlogioRedLight = const Color(0xFFFF6659);
 const kOrlogioRedDark = const Color(0xFF9A0007);
 
 const kOrlogioGreen = const Color(0xFFC7E5C8);
-const kOrlogioGreenLight = const Color(0xFFfAFFFB);
+const kOrlogioGreenLight = const Color(0xFFFAFFFB);
 const kOrlogioGreenDark = const Color(0xFF96B397);
