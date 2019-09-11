bb865e7816d44c8f8b42cb897470548870f78a87
Max Helmetag <mhelmetag@gmail.com>
2019-09-10 23:08:41 -0700

    remove some signing properties that aren't needed yet

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/max/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/max/projects/dart/orlogio"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/max/tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
